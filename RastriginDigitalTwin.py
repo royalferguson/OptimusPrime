bae08e92170e7a14afa40277da9f572ca25c3a9f
royalferguson <royferguson2016@gmail.com>
2020-06-10 16:14:34 -0500

    Updates

--- RastriginDigitalTwin.py
+++ RastriginDigitalTwin.py
@@ -7,9 +7,11 @@ import argparse, sys
 class RastriginDigitalTwin(AlgoDigitalTwin):
 
 		def __init__ (self):
-			# uncomment this when you want to provide x0
+
+			# Use this when you wish to provide the initial position (x0)
 			super().__init__(rastrigin, x0 = utils.get_random_x0(10,-5.12, 5.12))
-			# comment this when you want to provide x0
+
+			# Use this when you don't want to provide initial position (x0)
 			# super().__init__(rastrigin)
 
 		def optimize(self, args, **kwargs):
@@ -18,32 +20,32 @@ class RastriginDigitalTwin(AlgoDigitalTwin):
 if __name__ == '__main__':
 	args = cfg.get_commandline_args()
 
-	print("args:    ", args) 
-
-	
-	bnds = np.full((10,2), (-5.12, 5.12))
-	def callback_(x,f,accept):
-		print("custom callback for basinhopping")
-		return
 	# for basinhopping
-	kwargs = {'niter':2,
-	'T': 0.2,
-	'stepsize':0.65,
-	'minimizer_kwargs': {
-		'method':'BFGS'
-	},
-	'interval':2,
-	'disp':1,
-	'niter_success':2,
-	'accept_test': None,
-	'take_step': None,
-	'callback': callback_,
-	'seed': 20
-	}
-	# for pso
-	"""kwargs = {
+	if args.solver == 'basinhopping':
+
+		def callback_(x,f,accept):
+			print("custom callback for basinhopping")
+			return
+
+		kwargs = {'niter':2,
+		'T': 0.2,
+		'stepsize':0.65,
+		'minimizer_kwargs': {
+			'method':'BFGS'
+		},
+		'interval':2,
+		'disp':1,
+		'niter_success':2,
+		'accept_test': None,
+		'take_step': None,
+		'callback': callback_,
+		'seed': 20
+		}
+
+	elif args.solver == 'GlobalBestPSO':
+		kwargs = {
 		'dimensions':10,
-		'bounds':bnds,
+		'bounds': np.full((10,2), (-5.12, 5.12),
 		'maxiter':100,
 		'n_particles':200,
 		'options': {'c1':0.5,'c2': 0.7, 'w' : 0.85},
@@ -53,7 +55,7 @@ if __name__ == '__main__':
 						'center' : 2,
 						'ftol' : -1
 						}
-	}"""
+		}
 
 	if args.trace:
 		utils.run_with_callgraph(cfg.main, RastriginDigitalTwin(), args, **kwargs)
