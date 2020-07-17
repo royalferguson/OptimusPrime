6b0bc64f648b3320905910e8ff8369bb47cf1365
unknown <o.wagih.ow@gmail.com>
2020-07-17 20:08:14 +0200

    static x0

--- OptimusPrime/utils/__init__.py
+++ OptimusPrime/utils/__init__.py
@@ -1,4 +1,5 @@
-from .generate_dv import get_random_x0, get_random_multiple_boundaries
+from .generate_dv import get_random_x0, get_random_multiple_boundaries, get_static_x0
 from .callgraph import run_with_callgraph
 from .display import start_display_window, set_global_frame
-__all__=["get_random_x0", "run_with_callgraph",'start_display_window','set_global_frame', 'get_random_multiple_boundaries']
+__all__=["get_random_x0", "run_with_callgraph",'start_display_window','set_global_frame', 'get_random_multiple_boundaries',
+'get_static_x0']
